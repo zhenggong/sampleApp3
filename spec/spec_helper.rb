35bf059c8f6001b98a2f8e89faf23c4f7e8e7434
zheng-gong <zheng.gong@gree.net>
2015-05-05 16:50:07 +0900

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #Capybaraのvisitがundefinedと言われたとき、Capybara::DSLを含めるようにしたらうまくいった
+  config.include Capybara::DSL
+
 end
